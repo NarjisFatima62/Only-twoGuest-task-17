43db4c02a28b278d82285d31645fccaf6492026c
Narjis Fatima <161502591+NarjisFatima62@users.noreply.github.com>
2024-05-12 21:28:02 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,27 @@
+// Question 17:
+
+//Answer:
+
+//Initial list of guest:
+let guests : String[] = ["Zara", "asra", "Zoe", "Izhan", "Arham"];
+
+//Informing that only two people can be invited.
+//console.log("Due to limited spacee, only two people can be invited for dinner.");
+
+//Removing guest until only two people names remain.
+//while(guests.length > 2){
+    //onst removedGuest = guests.pop();// remove the last guest from the list.
+//console.log(`Sorry, ${removedGuest}, you're no longer invited to dinner `);
+//};
+
+//Printing invitations to the remaining two guest.
+//guests.forEach((guest) => {
+  //  console.log(`Dear ${guest}, you're still invited to dinner,`);
+//});
+
+// Removing the last two names from the list .
+guests.pop();
+guests.pop();
+
+//Printing the final list to confirm its empty:
+console.log("Final guest list;", guests);
